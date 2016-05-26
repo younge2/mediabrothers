507a67fe04e9467c691304f113dae3a64c8d0d8d
Ethan Y <young.e.f42@gmail.com>
2016-05-26 14:45:16 -0700

    More visuals. Set up for content email form

--- public/app/js/controllers.js
+++ public/app/js/controllers.js
@@ -2,8 +2,31 @@
 
 var ctrl = angular.module('MediaCtrl', []);
 
-ctrl.controller('Home', ['$scope', function($scope) {
-	$scope.where = 'Home Page'
+ctrl.controller('Home', ['$scope', function($scope, $http) {
+	$scope.where = 'Home Page';
+	$scope.MessageIn = "Message";
+
+	this.sendMail=function() {
+		var data = ({
+                InputName : this.InputName,
+                InputEmail : this.InputEmail,
+                InputPhone : this.InputPhone,
+                InputMessage : this.InputMessage
+            });
+
+		$http.post('/contact-form', data).
+                success(function(data, status, headers, config) {
+                    // this callback will be called asynchronously
+                    // when the response is available
+ 
+                    
+ 
+                }).
+                error(function(data, status, headers, config) {
+                    // called asynchronously if an error occurs
+                    // or server returns response with an error status.
+                });
+	}
 
 }]);
 
