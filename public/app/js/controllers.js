aae815ab144ff05d18148a5de958741aa3c95a6b
Ethan Y <young.e.f42@gmail.com>
2016-05-31 21:11:29 -0700

    Stupid ass refresh bug

--- public/app/js/controllers.js
+++ public/app/js/controllers.js
@@ -37,6 +37,45 @@ ctrl.controller('Home', ['$scope', '$http', function($scope, $http) {
 
 }]);
 
+ctrl.controller('Landing', ['$scope', '$http', function($scope, $http) {
+	$scope.where = 'LandingPage';
+	$scope.MessageIn = "Message";
+	$scope.data = [];
+	$scope.InputName = "";
+	$scope.InputEmail = "";
+	$scope.InputPhone = "";
+	$scope.InputMessage = "";
+
+	$scope.sendMail=function() {
+		 $scope.data = ({
+                InputName : $scope.InputName,
+                InputEmail : $scope.InputEmail,
+                InputPhone : $scope.InputPhone,
+                InputMessage : $scope.InputMessage
+            });
+
+		$http.post('/contact-form', $scope.data).
+                success(function(data, status, headers, config) {
+                    // this callback will be called asynchronously
+                    // when the response is available
+ 					console.log("success");
+ 					console.log($scope.data);
+                    
+ 
+                }).
+                error(function(data, status, headers, config) {
+                    // called asynchronously if an error occurs
+                    // or server returns response with an error status.
+                    console.log("error");
+                });
+	}
+
+}]);
+
 ctrl.controller('NavCtrl', ['$scope', '$state', function($scope, $state) {
+
+}]);
+
+ctrl.controller('MRPPC', ['$scope', '$state', function($scope, $state) {
 
 }]);
